fff876744f41608dbe969bff353bf8130473ac8a
Bohdan <codeberry1994@gmail.com>
2024-03-19 19:47:34 +0100

    add save database

--- car_scraper/pipelines.py
+++ car_scraper/pipelines.py
@@ -1,13 +1,83 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+import logging
+from datetime import datetime
 
+import psycopg2
+from dotenv import load_dotenv
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 
 class CarScraperPipeline:
+
+    def open_spider(self, spider):
+        logging.info("Connecting to the database")
+        self.connection = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        )
+
+        self.curr = self.connection.cursor()
+
     def process_item(self, item, spider):
+        self.curr.execute(''' 
+        CREATE TABLE IF NOT EXISTS cars (
+            id SERIAL PRIMARY KEY,
+            url TEXT,
+            title TEXT,
+            price_usd INTEGER,
+            odometer INTEGER,
+            username TEXT,
+            phone_number TEXT,
+            image_url TEXT,
+            car_number TEXT,
+            car_vin TEXT,
+            datetime_found TIMESTAMP
+        ) ''')
+
+        self.curr.execute('''
+            INSERT INTO cars (
+                url, 
+                title, 
+                price_usd, 
+                odometer, 
+                username, 
+                phone_number, 
+                image_url, 
+                car_number, 
+                car_vin, 
+                datetime_found
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ''', (
+            item['url'],
+            item['title'],
+            item['price_usd'],
+            item['odometer'],
+            item['username'],
+            item['phone_number'],
+            item['image_url'],
+            item['car_number'],
+            item['car_vin'],
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        ))
+
+        self.connection.commit()
+        logging.info("Data transferred to the PostgreSQL database")
         return item
+
+    def close_spider(self, spider):
+        self.curr.close()
+        logging.info("Closing database connection")
+        self.connection.close()
+
+
+
